b17aa8fd82a5a8f234658e17917d5fbd375bda44
akashKarmakar02 <akash.anan03@gmail.com>
2025-05-13 18:48:22 +0530

    build: create data directory and copy data files for Windows build

--- build-windows.sh
+++ build-windows.sh
@@ -1,6 +1,8 @@
 mkdir -p windows-build
 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -o windows-build/farmrpg.exe -ldflags "-s -w" 
 mkdir -p windows-build/assets
+mkdir -p windows-build/data
 cp -r assets/* windows-build/assets/
+cp -r data/* windows-build/data/
 cp raylib.dll windows-build/
 tar -czvf farmrpg_windows.tar.gz -C windows-build .
